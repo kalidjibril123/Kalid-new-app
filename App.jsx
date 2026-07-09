56ba3ff2dc358e6e99d4f9e23e4f33838dab896e
kalidjibril123 <kalidjibrilkalid@gmail.com>
2026-07-09 23:03:56 +0300

    App.jsx

---
+++ App.jsx
@@ -0,0 +1,24 @@
+import Header from "./components/Header";
+import OrderForm from "./components/OrderForm";
+import "./style.css";
+
+function App(){
+
+return(
+<div>
+<Header/>
+
+<h1>Kalid New App</h1>
+
+<p>
+Kalid Jibril Metal & Wood Work
+</p>
+
+<OrderForm/>
+
+</div>
+)
+
+}
+
+export default App;
